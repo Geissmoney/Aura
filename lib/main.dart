e7b33957334009c51e661ab46e26c841c6c8d57e
hugh5 <hughdrummond11@gmail.com>
2024-08-17 13:22:57 +1000

    quick fix

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'pages/home.dart';
+import 'ui/screens/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
