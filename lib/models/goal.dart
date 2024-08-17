55881f16366f4c0e60bce43d4eadc3f4d7d8ea2f
hugh5 <hughdrummond11@gmail.com>
2024-08-17 15:02:40 +1000

    model from firestore methods

--- lib/models/goal.dart
+++ lib/models/goal.dart
@@ -9,4 +9,12 @@ class Goal {
       {required this.goalType,
       required this.achievements,
       required this.target});
+
+  Goal.fromFirestore(Map<String, dynamic> data)
+      : goalType = data['goalType'] as int,
+        achievements =
+            (data['achievements'] as Iterable<dynamic>).map((achievement) {
+          return Achievement.fromFirestore(achievement as Map<String, dynamic>);
+        }).toList(),
+        target = data['target'] as double;
 }
