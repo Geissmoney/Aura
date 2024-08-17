c2acc12595f907b9bb4d2e00c1a7adf8c4446907
NIMESH GARG <51132740+nimeshgarg@users.noreply.github.com>
2024-08-17 16:08:27 +1000

    Basic Friends page

--- lib/ui/screens/friends.dart
+++ lib/ui/screens/friends.dart
@@ -10,6 +10,18 @@ class FriendsHome extends StatefulWidget {
 class _FriendsHomeState extends State<FriendsHome> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child:Column(
+        children: [
+          Padding(padding: EdgeInsets.all(20.0)),
+          CircleAvatar(
+            radius: 100,
+            backgroundImage: AssetImage('assets/images/avatar.png'),
+          ),
+          Padding(padding: EdgeInsets.all(5.0)),
+          Text("User Name", style: TextStyle(fontSize: 30),),
+        ],
+      )
+    );
   }
 }
