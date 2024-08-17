915ef30debf7594ccb618a64ea58985af25bc02c
hugh5 <hughdrummond11@gmail.com>
2024-08-17 16:58:46 +1000

    database stuff

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,12 +1,37 @@
-import 'package:aura/models/day.dart';
-
 class UserModel {
   String name;
-  List<Day> days;
+  int aura;
 
-  UserModel({required this.name, required this.days});
+  UserModel({required this.name, this.aura = 0});
 
   UserModel.fromFirestore(Map<String, dynamic> data)
-      : name = data['display_name'],
-        days = data['days'];
+      : name = data['name'],
+        aura = data['aura'] as int;
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'name': name,
+      'aura': aura,
+    };
+  }
+}
+
+class Friend {
+  String status;
+
+  Friend({required this.status});
+
+  Friend.fromFirestore(Map<String, dynamic> data) : status = data['status'];
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'status': status,
+    };
+  }
+}
+
+enum FriendStatus {
+  sent,
+  received,
+  friend,
 }
